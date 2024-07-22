0aa1633c513744fe86579bf754430eb09279fdbd
Naman <namansngh64@gmail.com>
2024-07-22 23:25:20 +0530

    updated render allowedhost

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost','*.onrender.com']
+ALLOWED_HOSTS = ['localhost','recipe-api-pyjamahr-1.onrender.com']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
